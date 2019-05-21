ffc8a438d76db94c77d13c44e16b3935ede4c620
Cai Yang <caih.yang@gmail.com>
2019-05-21 16:49:08 -0400

    done

--- main.rb
+++ main.rb
@@ -1,38 +1,97 @@
 class Word
-  # Stores the provided word in an instance variable and
-  # setups up any other instance variables.
+  attr_reader :word
+
   def initialize(word)
+    @word = word.downcase.split("").map{|letter| [letter,false]}
   end
 
-  # Guess a letter in the word.
-  # Should return a boolean. true if the letter is in the word, false if not.
-  def guess!(letter)
+  def guess?(letter)
+    changed = false
+    @word.each do |letter_bool|
+      if letter_bool[0] == letter
+        letter_bool[1] = true
+        changed=true
+      end
+    end
+    changed
   end
 
-  # Whether all letters in the word have been guessed correctly.
   def guessed_all_correct?
+    @word.each do |letter_bool|
+      if letter_bool[1] == false
+        return false
+      end
+    end
+    return true
   end
 
-  # Display the current state of the guessed word for the player.
   def output_word
+    output = ""
+    @word.each do |letter_bool|
+      if letter_bool[1] == true
+        output += letter_bool[0] + " "
+      else
+        output += "_ "
+      end
+    end
+    print output
   end
 
-  # Check whether a provided character is a letter
   def self.is_letter?(character)
   end
 end
 
 class MysteryWordGame
-  # Initialize any state in the game, and start the game loop.
+
   def initialize
+    ask_for_word
   end
 
   def ask_for_word
+    puts "Gimme a word"
+    word = gets.chomp
+    @secret_word = Word.new(word)
+    @lives = 3
+    game_loop
   end
 
   # Run the game loop, which continues until the player wins or loses.
   def game_loop
+
+    while @secret_word.guessed_all_correct? == false && @lives > 0
+
+      puts "Guess a letter: "
+      @secret_word.output_word
+      letter = gets.chomp
+
+      while letter.length != 1 || letter.match(/[0-9]/)
+        puts "Invalid Input"
+        puts "Try Again: "
+        @secret_word.output_word
+        letter = gets.chomp
+      end
+
+
+      if @secret_word.guess?(letter)
+        puts "#{letter} exist"
+      else
+        @lives -= 1;
+        puts "#{letter} does not exist"
+        puts "You have #{@lives} left"
+
+        if @lives == 0
+          puts "You lose"
+          return
+        end
+
+      end
+
+    end # while loop
+
+    @secret_word.output_word
+    puts "\nFinally, You win!"
   end
 end
 
-MysteryWordGame.new
+game = MysteryWordGame.new
+# game.ask_for_word
